617fa7359a4e6349673383eee4ea9cd5402dfcf8
DaveX101 <dxiang1@gmail.com>
2014-12-02 16:42:02 -0500

    init xcode project

---
+++ README.md
@@ -0,0 +1,9 @@
+<h2>Wildcard iOS SDK</h2>
+
+<h3>Building</h3>
+
+<h4>Building WildcardSDK.framework</h4>
+
+* Open up WildcardSDK/WildcardSDK.xcodeproj
+* Select scheme **Universal** for **iOS Device**
+* **WildcardSDK.framework** should be outputted in project directory and desktop
---
+++ WildcardSDKProject/WildcardSDKTestApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WildcardSDKTestApp
+//
+//  Created by David Xiang on 12/2/14.
+//
+//
+
+import UIKit
+import WildcardSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(WildcardSDKVersionNumber)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
