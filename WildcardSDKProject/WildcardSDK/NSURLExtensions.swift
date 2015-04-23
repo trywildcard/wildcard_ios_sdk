be7553ef9083fc5f2fbc67e2e22c2f3ef7ca8785
DaveX101 <dxiang1@gmail.com>
2015-04-23 15:29:52 -0400

    couple extension functions to match twitter urls

---
+++ WildcardSDKProject/WildcardSDK/NSURLExtensions.swift
@@ -0,0 +1,46 @@
+//
+//  NSURLExtensions.swift
+//  WildcardSDKProject
+//
+//  Created by David Xiang on 4/23/15.
+//
+//
+
+import Foundation
+
+public extension NSURL{
+    
+    public func isTwitterProfileURL()->Bool{
+        if(absoluteString != nil){
+            let pattern = "^http(s)://(www.)twitter.com/(\\w*)\\/?$"
+            let regex = NSRegularExpression(pattern: pattern, options: NSRegularExpressionOptions.CaseInsensitive, error: nil)
+            let length:Int = absoluteString!.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
+            let ytMatch = regex?.firstMatchInString(absoluteString!, options: NSMatchingOptions.allZeros, range: NSMakeRange(0, length))
+            if(ytMatch != nil){
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }
+    
+    public func isTwitterTweetURL()->Bool{
+        if(absoluteString != nil){
+            let pattern = "^http(s)://(www.)twitter.com/(\\w*)/status/(\\d*)\\/?$"
+            let regex = NSRegularExpression(pattern: pattern, options: NSRegularExpressionOptions.CaseInsensitive, error: nil)
+            let length:Int = absoluteString!.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
+            let ytMatch = regex?.firstMatchInString(absoluteString!, options: NSMatchingOptions.allZeros, range: NSMakeRange(0, length))
+            if(ytMatch != nil){
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }
+    
+    
+}
---
+++ WildcardSDKProject/WildcardSDKTests/WildcardSDKExtensionTests.swift
@@ -0,0 +1,60 @@
+//
+//  WildcardSDKExtensionTests.swift
+//  WildcardSDKProject
+//
+//  Created by David Xiang on 4/23/15.
+//
+//
+
+import UIKit
+import XCTest
+import WildcardSDK
+
+class WildcardSDKExtensionTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testTwitterProfileURL() {
+        var url:NSURL?
+        url = NSURL(string: "https://www.twitter.com/maxbulger")
+        XCTAssert(url?.isTwitterProfileURL() == true)
+        url = NSURL(string: "https://www.twitter.com/maxbulger/")
+        XCTAssert(url?.isTwitterProfileURL() == true)
+        url = NSURL(string: "https://www.twitter.com/maxbulger/status/23234234/")
+        XCTAssert(url?.isTwitterProfileURL() == false)
+        url = NSURL(string: "https://www.facebook.com/maxbulger")
+        XCTAssert(url?.isTwitterProfileURL() == false)
+        url = NSURL(string: "https://www.facebook.com/maxbulger/asdfa/asdfa")
+        XCTAssert(url?.isTwitterProfileURL() == false)
+    }
+    
+    func testTwitterTweetURL(){
+        var url:NSURL?
+        url = NSURL(string: "https://www.twitter.com/maxbulger")
+        XCTAssert(url?.isTwitterTweetURL() == false)
+        url = NSURL(string: "https://www.twitter.com/maxbulger/")
+        XCTAssert(url?.isTwitterTweetURL() == false)
+        url = NSURL(string: "https://www.twitter.com/maxbulger/status/23234234/")
+        XCTAssert(url?.isTwitterTweetURL() == true)
+        url = NSURL(string: "https://www.twitter.com/maxbulger/status/23234234")
+        XCTAssert(url?.isTwitterTweetURL() == true)
+        url = NSURL(string: "https://www.facebook.com/maxbulger")
+        XCTAssert(url?.isTwitterTweetURL() == false)
+        url = NSURL(string: "https://www.facebook.com/maxbulger/asdfa/asdfa")
+        XCTAssert(url?.isTwitterTweetURL() == false)
+        url = NSURL(string: "https://www.facebook.com/maxbulger/status/notnumbers")
+        XCTAssert(url?.isTwitterTweetURL() == false)
+        url = NSURL(string: "https://www.facebook.com/maxbulger/status/notnumbers/")
+        XCTAssert(url?.isTwitterTweetURL() == false)
+        
+    }
+
+}
